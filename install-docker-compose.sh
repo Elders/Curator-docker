af83b7a19cb6aaad5813b7e83eb729400e85771f
SayliS <kalin.venkov@eldersoss.com>
2018-01-05 16:25:42 +0200

    Fixes creation of directory

--- install-docker-compose.sh
+++ install-docker-compose.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-mkdir /opt/bin/
+mkdir -p /opt/bin/
 curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /opt/bin/docker-compose
 chmod +x /opt/bin/docker-compose
