02232461f69ebf60753a2fb10936944840f07c5f
SayliS <kalin.venkov@eldersoss.com>
2018-01-05 15:19:06 +0200

    Making sure that directory exists

--- install-docker-compose.sh
+++ install-docker-compose.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+mkdir /opt/bin/
 curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /opt/bin/docker-compose
 chmod +x /opt/bin/docker-compose
