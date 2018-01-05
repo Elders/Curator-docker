6d23e723b0153382c1d9537d9b841cca6ac5c712
SayliS <kalin.venkov@eldersoss.com>
2018-01-05 14:50:00 +0200

    Adds setting of vm.max_map_count

--- install-log.sh
+++ install-log.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 mkdir -p /compose/log
 curl -L https://raw.githubusercontent.com/Elders/Curator-docker/release-3.0.0/docker-compose.yaml -o /compose/log/docker-compose.yaml
+echo "$1" | sudo sysctl -w vm.max_map_count=262144
 echo "$1" | sudo docker-compose --file /compose/log/docker-compose.yaml up -d
